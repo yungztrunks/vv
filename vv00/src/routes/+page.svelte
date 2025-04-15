4ffaafd96285c66ab41e1a8ff7149dbc5407f0a6
yungztrunks <silvanbelten@gmail.com>
2025-04-15 14:52:11 +0200

    folders listing page

--- vv00/src/routes/+page.svelte
+++ vv00/src/routes/+page.svelte
@@ -1,2 +1,13 @@
+<script lang="ts">
+    export let data;
+    const { folders } = data;
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+{#each folders as folder}
+    <div>
+        <a href={`/${folder.slug}`}>{folder.name}</a>
+    </div>
+{/each}
