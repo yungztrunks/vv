3a4123e63895a92c552f475bde6aba3410eb57dd
yungztrunks <silvanbelten@gmail.com>
2025-04-15 14:53:13 +0200

    rewritten home page for now

--- vv00/src/routes/+page.svelte
+++ vv00/src/routes/+page.svelte
@@ -6,8 +6,12 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
-{#each folders as folder}
-    <div>
-        <a href={`/${folder.slug}`}>{folder.name}</a>
-    </div>
-{/each}
+{#if folders.length === 0}
+    <p>i am empty.</p>
+{:else}
+    {#each folders as folder}
+        <div>
+            <a href={`/${folder.slug}`}>{folder.name}</a>
+        </div>
+    {/each}
+{/if}
