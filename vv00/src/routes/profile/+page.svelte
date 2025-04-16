12aa9fddc5fea3540e4daa2341653ec698fa763f
yungztrunks <silvanbelten@gmail.com>
2025-04-16 16:23:51 +0200

    profile page

---
+++ vv00/src/routes/profile/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import {pb, currentUser, logout} from '$lib/pocketbase';
+    import {goto} from '$app/navigation';
+
+    let user = currentUser;
+
+    async function signout() {
+        await logout();
+    }
+</script>
+
+<h1>Profile Page</h1>
+
+<p>Signed in as {$user?.username}</p>
+<button on:click={signout}>Sign out</button>
