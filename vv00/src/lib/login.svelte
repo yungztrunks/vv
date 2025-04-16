b491d8e4ccabb86422ea6ec6ddfc17d9bf3fba80
yungztrunks <silvanbelten@gmail.com>
2025-04-16 16:22:52 +0200

    login component

---
+++ vv00/src/lib/login.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+    import { pb, currentUser } from "$lib/pocketbase";
+    import {goto} from "$app/navigation";
+    
+    let username: string;
+    let email: string;
+    let password: string;
+    
+    async function login() {
+        try{
+            await pb.collection("users").authWithPassword(username, password);
+            goto("/profile");
+        }
+        catch(error) {
+            console.error("login error", error);
+            alert("failed to login");
+        }
+    }
+    
+    async function signup() {
+        try {
+            const data = {
+                username,
+                email,
+                password,
+                passwordConfirm: password,
+                role: "pending",
+            };
+            const created_user = await pb.collection("users").create(data);
+            await login();
+        } catch (error) {
+            console.error("signup error", error);
+            alert("failed to signup");
+        }
+    }
+
+    function signout() {
+        pb.authStore.clear();
+    }
+</script>
+
+<p>Please log in.</p>
+<form on:submit|preventDefault>
+    <input placeholder="username" bind:value={username} type="text" required />
+    <input placeholder="password" bind:value={password} type="password" required />
+    <button on:click={login}>Login</button>
+</form>
+
+<p>or sign up</p>
+<form on:submit|preventDefault>
+    <input placeholder="username" bind:value={username} type="text" required />
+    <input placeholder="email" bind:value={email} type="email" required />
+    <input placeholder="password" bind:value={password} type="password" required />
+    <button on:click={signup}>Sign Up</button>
+</form>
