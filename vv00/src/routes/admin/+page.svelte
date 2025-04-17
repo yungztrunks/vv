7b08e87f935c309980bba5d71c80087c405b2c17
*weshford <silvanbelten@gmail.com>
2025-04-17 21:28:08 +0200

    admin panel

---
+++ vv00/src/routes/admin/+page.svelte
@@ -0,0 +1,8 @@
+<h1>Admin Panel</h1>
+<nav>
+    <ul>
+        <li><a href="/admin/users">User Management</a></li>
+        <li><a href="/admin/posts">Post Management</a></li>
+        <li><a href="/admin/folders">Folder Management</a></li>
+    </ul>
+</nav>
